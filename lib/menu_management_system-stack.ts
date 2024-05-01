23cd880a6b8d94eee5e619ecd6ee446a144be450
shin <sshoony95@gmail.com>
2024-05-01 15:37:03 -0400

    Add functions to crud users

--- lib/menu_management_system-stack.ts
+++ lib/menu_management_system-stack.ts
@@ -1,6 +1,8 @@
-import { StackProps, Aws, Stack, RemovalPolicy, CfnOutput } from "aws-cdk-lib";
+import { StackProps, Aws, Stack, RemovalPolicy, CfnOutput, Duration, Tags } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as lambda_nodejs from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class MenuManagementSystemStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -16,6 +18,29 @@ export class MenuManagementSystemStack extends Stack {
     new CfnOutput(this, "UsersTable", {
       description: "DynamoDB Users table",
       value: usersTable.tableName,
+    });
+
+    const nodejsFunctionProps = {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      memorySize: 128,
+      timeout: Duration.seconds(100),
+      tracing: lambda.Tracing.ACTIVE,
+    };
+
+    const usersFunction = new lambda_nodejs.NodejsFunction(this, 'users-function', {
+      entry: './src/api/users/index.ts',
+      handler: 'handler',
+      ...nodejsFunctionProps,
+      environment: {
+        USERS_TABLE: usersTable.tableName,
+      },
+    });
+    usersTable.grantReadWriteData(usersFunction);
+    Tags.of(usersFunction).add('Stack', `${Aws.STACK_NAME}`);
+
+    new CfnOutput(this, 'UsersFunction', {
+      description: 'Lambda function used to perform actions on the users data',
+      value: usersFunction.functionName,
     });
   }
 }
